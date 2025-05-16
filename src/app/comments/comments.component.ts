5eb1f7baf6f45b1aad82a5cd9c3885a21dab1e7b
LUCHER4321 <lucher4321@gmail.com>
2025-05-16 16:38:18 -0400

    Create comments.component.ts

---
+++ src/app/comments/comments.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comments',
+  imports: [],
+  template: `
+    <h3>Comentarios de lo que sea</h3>
+    <img src="https://img2.rtve.es/i/?w=1600&i=1614352806474.png"/>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut sapiente praesentium deserunt dolor ullam modi iste tenetur deleniti ab quod et mollitia, quis magni architecto explicabo, assumenda, provident sit necessitatibus.</p>
+  `,
+  styles: `
+    img {
+      width: 100%;
+      height: auto;
+    }`,
+})
+export class CommentsComponent {
+
+}
